04b552bb407b2fe4448db723208515ed1911a523
muhstefan <muhstefang@gmail.com>
2025-07-12 23:13:23 +0700

    rawg optimisation

--- gamescore/api_v1/rawg/views.py
+++ gamescore/api_v1/rawg/views.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from gamescore.api_v1.rawg.client import fetch_games_from_rawg
+from gamescore.api_v1.rawg.service import parse_games
 from gamescore.core.db import get_db
 from gamescore.api_v1.games.crud import create_games
-import httpx
 import os
-from gamescore.core.models.games import GameCreate
 
 router = APIRouter(tags=["RAWG"])
 
@@ -13,35 +14,16 @@ RAWG_BASE_URL = "https://api.rawg.io/api"
 
 @router.get("/games/")
 async def get_games(session: AsyncSession = Depends(get_db)):
-
     params = {
         "key": RAWG_API_KEY,
         "ordering": "-rating",
-        "page_size": 250,
+        "page_size": 40,
         "platforms": "4"
     }
-
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"{RAWG_BASE_URL}/games", params=params)
-        if response.status_code != 200:
-            raise HTTPException(status_code=502, detail="Ошибка при запросе к RAWG API")
-        raw_data = response.json()
-
-        games_to_create = []
-        for raw_game in raw_data.get("results", []):
-            # Берём только name и image (image — обычно в поле background_image)
-            name = raw_game.get("name")
-            image = raw_game.get("background_image")
-
-            if not name:
-                continue  # пропускаем, если нет имени
-
-            game_in = GameCreate(
-                name=name,
-                image=image
-            )
-            games_to_create.append(game_in)
-
-        created_games = await create_games(session=session, games_in=games_to_create)
-
-        return {"created": len(created_games)}
+    try:
+        raw_games = await fetch_games_from_rawg(params, RAWG_BASE_URL)
+    except Exception:
+        raise HTTPException(status_code=502, detail="Ошибка при запросе к RAWG API")
+    games_to_create = parse_games(raw_games)
+    created_games = await create_games(session=session, games_in=games_to_create)
+    return {"created": len(created_games)}
