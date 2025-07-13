4a2da6cb6ac5014638e0ecafc2988cb53bad2f7a
muhstefan <muhstefang@gmail.com>
2025-07-13 21:27:29 +0700

    UserGames table Many-to-Many first Api \ preload for user (relationships)

--- gamescore/api_v1/users/crud.py
+++ gamescore/api_v1/users/crud.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession # это сессия для работы с бд
+from sqlalchemy.orm import selectinload
+
 from gamescore.core.models import User
 from sqlalchemy.engine import Result
 from sqlalchemy import select
 from gamescore.core.models.users import UserCreateDB
+from gamescore.api_v1.games.crud import get_game
 
 async def get_users(session : AsyncSession) -> list[User]:
     stmt = select(User).order_by(User.id)
@@ -10,8 +13,12 @@ async def get_users(session : AsyncSession) -> list[User]:
     users = result.scalars().all()  # scalars аналог **
     return list(users)
 
-async def get_user(session: AsyncSession,user_id: int)-> User | None:
-    return await session.get(User, user_id)
+#Это метод пред загрузки пользователя И сразу всех его связей, так надо чтобы от ленивой загрузки не тормозить
+async def get_user(session: AsyncSession, user_id: int) -> User | None:
+    result = await session.execute(
+        select(User).options(selectinload(User.games)).where(User.id == user_id)
+    )
+    return result.scalars().first()
 
 async def create_user(session: AsyncSession, user_data: UserCreateDB):
     user = User(**user_data.model_dump())
@@ -30,11 +37,21 @@ async def update_user(
     await session.commit()
     return user
 
-
-
 async def delete_user(session: AsyncSession,
                       user_id: int
                       ) -> None:
     user = await get_user(session, user_id)
     await session.delete(user)
     await session.commit()
+
+async def add_game_to_user(session: AsyncSession,
+                           user_id: int,
+                           game_id: int):
+    user = await get_user(session, user_id)
+    game = await get_game(session, game_id)
+    if not user or not game:
+        raise ValueError("Пользователь или игра не найдены")
+    if game not in user.games:
+        user.games.append(game)
+        session.add(user)
+        await session.commit()
--- gamescore/api_v1/users/views.py
+++ gamescore/api_v1/users/views.py
@@ -3,18 +3,18 @@ from gamescore.core.models.users import UserCreateDB, UserUpdate, User
 from sqlalchemy.ext.asyncio import AsyncSession
 from . import crud
 from .dependencies import user_by_id, prepare_user_create, prepare_user_update
-from gamescore.core.models import db_helper
+from gamescore.core.db import get_db
 
 router = APIRouter(tags=["Users"])
 
 @router.get("/", response_model=list[User])
-async def get_users(session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+async def get_users(session : AsyncSession = Depends(get_db)):
     return await crud.get_users(session=session)
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def create_user(
     user_data: UserCreateDB = Depends(prepare_user_create),
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+    session : AsyncSession = Depends(get_db)
 ):
     user = await crud.create_user(session, user_data)
     return user
@@ -28,7 +28,7 @@ async def update_user(
     user_id: int,
     user_update: UserUpdate,
     update_data: dict = Depends(prepare_user_update),
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+    session : AsyncSession = Depends(get_db)
 ):
     updated_user = await crud.update_user(session, user_id, update_data)
     return updated_user
@@ -38,7 +38,7 @@ async def update_user_partial(
     user_id: int,
     user_update: UserUpdate,
     update_data: dict = Depends(prepare_user_update),
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+    session : AsyncSession = Depends(get_db)
 ):
     updated_user = await crud.update_user(session, user_id, update_data)
     return updated_user
@@ -46,6 +46,15 @@ async def update_user_partial(
 @router.delete("/{user_id}/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
     user_id: int,
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+    session : AsyncSession = Depends(get_db)
 ) -> None:
     await crud.delete_user(session, user_id)
+
+
+@router.post("/{user_id}/games/{game_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def add_game_to_user(
+    user_id: int,
+    game_id: int,
+    session : AsyncSession = Depends(get_db)
+):
+    await crud.add_game_to_user(session, user_id, game_id)
