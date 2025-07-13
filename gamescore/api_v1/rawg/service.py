6a12588f02fbf5abf1832ea2b033458f5dfd9966
muhstefan <muhstefang@gmail.com>
2025-07-13 09:30:46 +0700

    Games erotic filtration

--- gamescore/api_v1/rawg/service.py
+++ gamescore/api_v1/rawg/service.py
@@ -1,10 +1,20 @@
 from gamescore.core.models.games import GameCreate
 
+excluded_tags = {"romance" , "nsfw", "adult", "erotic"}
+
 def parse_games(raw_games: list[dict]) -> list[GameCreate]:
     games = []
     for raw_game in raw_games:
         name = raw_game.get("name")
         image = raw_game.get("background_image")
-        if name:
+
+        rating = raw_game.get("esrb_rating")
+        rating_id = rating.get("id") if rating else None
+
+        tags = raw_game.get("tags", [])
+        game_tag_names = {tag.get("name", "").lower() for tag in tags}
+
+        if name and rating_id != 5 and not game_tag_names.intersection(excluded_tags):
             games.append(GameCreate(name=name, image=image))
+
     return games
